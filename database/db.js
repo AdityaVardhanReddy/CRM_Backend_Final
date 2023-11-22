66b1e679ef5d737214247a27789be0e5b4ffd687
unknown <pavr.0606@gmail.com>
2023-11-22 13:29:55 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const Connection = async () => {
+    
+
+    try {
+        await mongoose.connect('mongodb+srv://somaudaykiran:Uday@cluster0.4ek7fgv.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+          
+        console.log('Database Connected Succesfull');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+}
+
+export default Connection;
